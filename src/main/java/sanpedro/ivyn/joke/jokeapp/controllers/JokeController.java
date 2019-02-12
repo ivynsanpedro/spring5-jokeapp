a4332d61fd703872e762c1c6e76082eb909a91e3
Ivyn San Pedro <isanpedro@gmail.com>
2019-02-12 14:43:59 -0500

    Add JokeController.

---
+++ src/main/java/sanpedro/ivyn/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package sanpedro.ivyn.joke.jokeapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sanpedro.ivyn.joke.jokeapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
